0b7e8e8bd9f124326f1e3d41df91cd04a2494f42
Bob Lee <Bob@Bobs-MacBook-Pro.local>
2016-12-18 17:04:03 +0900

    Bob the Developer

--- CustomTable/ViewController.swift
+++ CustomTable/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  var socialMediaNameArray = ["Facebook", "Medium", "LinkedIn", "Twitter", "YouTube"]
+  var usernameArray = ["Bob the Develeoper", "Bob Lee", "Bob Lee", "@bobleesj", "Bob the Developer"]
+  var socialMediaImageArray = [UIImage(named: "Facebook"), UIImage(named: "Medium"), UIImage(named: "LinkedIn"), UIImage(named: "Twitter"), UIImage(named: "YouTube")]
+
+  @IBOutlet weak var tableView: UITableView!
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+  
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return socialMediaNameArray.count
+  }
+  
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
+    
+    cell.usernameLabel.text = usernameArray[indexPath.row]
+    cell.nameLabel.text = socialMediaNameArray[indexPath.row]
+    cell.socialImage.image = socialMediaImageArray[indexPath.row]
+    return cell
+  }
+  
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 50
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
+  
 
 }
-
